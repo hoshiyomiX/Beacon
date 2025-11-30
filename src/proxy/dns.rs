91aefe2b0938d17c7d9a74f8d0fc3a39c96ebf82
hoshiyomiX <katahiranaruse.daydream@gmail.com>
2025-12-01 00:24:32 +0800

    refactor: migrate dns.rs from reqwest to worker::Fetch for DoH requests

--- src/proxy/dns.rs
+++ src/proxy/dns.rs
@@ -1,23 +1,18 @@
 use anyhow::Result;
-use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, CONTENT_TYPE};
-use reqwest::Client;
+use worker::{Fetch, Request, RequestInit, Headers, Method};
 
 pub async fn doh(req_wireformat: &[u8]) -> Result<Vec<u8>> {
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        CONTENT_TYPE,
-        HeaderValue::from_static("application/dns-message"),
-    );
-    headers.insert(ACCEPT, HeaderValue::from_static("application/dns-message"));
-    let client = Client::new();
-    let response = client
-        .post("https://1.1.1.1/dns-query")
-        .headers(headers)
-        .body(req_wireformat.to_vec())
-        .send()
-        .await?
-        .bytes()
-        .await?;
+    let mut headers = Headers::new();
+    headers.set("content-type", "application/dns-message")?;
+    headers.set("accept", "application/dns-message")?;
+    
+    let mut init = RequestInit::new();
+    init.with_method(Method::Post)
+        .with_headers(headers)
+        .with_body(Some(req_wireformat.to_vec().into()));
 
-    Ok(response.to_vec())
+    let request = Request::new_with_init("https://1.1.1.1/dns-query", &init)?;
+    let mut response = Fetch::Request(request).send().await?;
+    let bytes = response.bytes().await?;
+    Ok(bytes)
 }
